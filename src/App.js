11493772737630f942acc9befdcb372dd39dfd58
ggichuru <ggichuru.dev@gmail.com>
2021-11-09 19:58:57 +0300

    Setup and local hardhat dev env

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {useState} from 'react'
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+function App() {
+    // Store greeting in local state
+    const [greeting, setGreetingValue] = useState()
+
+    // request access to user's metamask account
+    async function requestAccount() {
+        await window.ethereum.request({
+            method: 'eth_requestAccounts'
+        })
+    }
+
+    // call the smart contract, read the current greeting value
+    async function fetchGreeting() {
+        if (typeof window.ethereum !== 'undefined') {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+
+            try {
+                const data = await contract.greet()
+                console.log('data: ', data)
+            } catch (error) {
+                console.log('Error: ', error)
+            }
+        }
+    }
+
+    // Call the smart contract, send an update
+    async function setGreeting() {
+        if (!greeting) return
+        if (typeof window.ethereum !== 'undefined') {
+            await requestAccount()
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner()
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+            const transation = await contract.setGreeting(greeting)
+
+            await transation.wait()
+            fetchGreeting()
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={fetchGreeting}>Fetch Greeting</button>
+                <button onClick={setGreeting}>Set Greeting</button>
+                <input onChange={
+                    e => setGreetingValue(e.target.value)
+                } 
+                placeholder="Set greeting"
+                />
+            </header>
+        </div>
+    )
+}
+
+export default App;
